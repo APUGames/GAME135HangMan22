1a07180c5f506e357f8bc9d68373c634a49bdccd
Jesus Noland <jesus.noland@litpen.com>
2023-02-06 22:41:28 -0800

    Getting player input

--- HangMan22/HangMan22/Program.cs
+++ HangMan22/HangMan22/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/**
+ * Author: Jesus Noland
+ * Creation date: 02-06-2023
+ * Purpose: Hangman game
+ */
+using System;
+
+namespace HangMan
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerInput;
+            playerInput = Console.ReadLine();
+
+            char? playerChoice;
+            if (playerInput == null && playerInput != "")
+            {
+                playerChoice = playerInput?[0];
+            }
+            else
+            {
+                playerChoice = '*';
+            }
+            
+
+            Console.WriteLine(playerChoice);
+        }
+    }
+}
